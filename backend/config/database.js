76eb683831fa2d68ba47a359f992332c48069ec6
Md. Shahin Alam <shahin.shohag6452@gmail.com>
2026-09-13 23:48:56 +0600

    Update MongoDB connection URI environment variable

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ConnectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(
       `✅ MongoDB connected successfully to: ${conn.connection.host}`
     );
